bfb0144cfb912ac64f8a48cfc0363b6fb4e5c10f
Aleksandr <genkonst5@gmail.com>
2024-01-31 14:33:29 +0300

    add IdentityServer logic

--- PSP-Backend/PSP.AuthService/Config.cs
+++ PSP-Backend/PSP.AuthService/Config.cs
@@ -1,3 +1,4 @@
+using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace PSP.AuthService;
@@ -13,18 +14,62 @@ public static class Config
                 ClientId = "passengerClient",
                 ClientSecrets = { new Secret("passengerClientSecret".Sha256()) },
                 AllowedScopes = { "api" },
+            },
+            new()
+            {
+                ClientId = "postman",
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+                RequireClientSecret = false,
+                RedirectUris = { "https://oauth.pstmn.io/v1/callback" },
+                AllowedScopes = 
+                {
+                    "api",
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile 
+                },
+                AllowAccessTokensViaBrowser = true,
+                AllowOfflineAccess = true,
+                RequireConsent = false
+            },
+            new()
+            {
+                ClientId = "angular",
+
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+                RequireClientSecret = false,
+
+                RedirectUris = { "http://localhost:4200" },
+                PostLogoutRedirectUris = { "http://localhost:4200" },
+                AllowedCorsOrigins = { "http://localhost:4200" },
+
+                AllowedScopes = {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    "api",
+                },
+
+                AllowAccessTokensViaBrowser = true,
+                RequireConsent = false,
             }
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new List<ApiScope>
         {
-            new ApiScope(){Name = "api"},
+            new(){Name = "api"},
         };
     
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new List<IdentityResource>{};
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
     
     public static IEnumerable<ApiResource> ApiResources =>
-        new List<ApiResource>{};
+        new List<ApiResource>
+        {
+            // new ApiResource("api")
+        };
 }
