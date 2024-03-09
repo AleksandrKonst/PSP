da5c7e66b303add8066a1c9e497cb79aec0c549a
Aleksandr <genkonst5@gmail.com>
2024-03-09 12:46:03 +0300

    add CSV load

--- PSP-Backend/PSP.AuthWebApi/AuthWebApi/Controllers/ClaimController.cs
+++ PSP-Backend/PSP.AuthWebApi/AuthWebApi/Controllers/ClaimController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using AuthWebApi.DTO;
 using AuthWebApi.DTO.ViewModels.Claim;
 using AuthWebApi.Models.Data;
@@ -38,6 +39,22 @@ public class ClaimController(AuthDbContext context, IMapper mapper, ILogger<Auth
         return View(indexViewModel);
     }
 
+    [HttpPost]
+    public async Task<IActionResult> ExportCsv()
+    {
+        var claims = await context.UserClaims.Distinct().ToListAsync();
+
+        var builder = new StringBuilder();
+        builder.AppendLine("UserId,ClaimType,ClaimValue");
+
+        foreach (var claim in claims)
+        {
+            builder.AppendLine($"{claim.UserId},{claim.ClaimType},{claim.ClaimValue}");
+        }
+        
+        return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "claims.csv");
+    }
+    
     [HttpGet]
     public async Task<IActionResult> Edit(int id)
     {
